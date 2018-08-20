5201507f164f41d06e487bf28b3642e5f161baff
Atsushi <atsushi@moongift.jp>
2018-08-20 16:32:19 +0900

    Add module

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const options = {
 };
 
 Bleacon.on('discover', function(bleacon) {
-   if (bleacon.uuid == options.uuid &&  bleacon.proximity != options.proximity) {
-     console.log(`ステータスが変わりました -> ${bleacon.proximity}`);
-     options.proximity = bleacon.proximity;
-   }
+  console.log(bleacon.uuid, options.uuid);
+  console.log(bleacon.proximity, options.proximity);
+  if (bleacon.uuid == options.uuid && bleacon.proximity != options.proximity) {
+    console.log(`ステータスが変わりました -> ${bleacon.proximity}`);
+    options.proximity = bleacon.proximity;
+  }
 });
 
 console.log('Started.')
